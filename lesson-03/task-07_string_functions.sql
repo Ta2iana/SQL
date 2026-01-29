e7a57ffaf11cb64f25d030ecd2a69fe0557d631c
Ta2iana <samofalova.ts@gmail.com>
2026-01-29 21:28:03 +0300

    Create task-07_string_functions.sql

---
+++ lesson-03/task-07_string_functions.sql
@@ -0,0 +1,9 @@
+-- Преобразование названия товара: взять первое слово в верхнем регистре
+-- Используются: SPLIT_PART, UPPER, ORDER BY
+-- Результат: name, first_word, price
+
+SELECT name,
+       price,
+       upper(split_part(name, ' ', 1)) as first_word
+FROM   products
+ORDER BY name
