63b34c9433c632b870db1a1a48a30d63088ec3b1
Ta2iana <samofalova.ts@gmail.com>
2026-01-29 21:59:08 +0300

    Create task-11_coalesce_birth.sql

---
+++ lesson-03/task-11_coalesce_birth.sql
@@ -0,0 +1,7 @@
+-- Задача 11 (урок 3, Карпов курсы SQL): COALESCE для birth_year с NULL -> 'unknown'
+-- id курьеров + birth_year из couriers (COALESCE + date_part::text)
+
+SELECT courier_id,
+       coalesce(date_part('year', birth_date)::text, 'unknown') as birth_year
+FROM   couriers
+ORDER BY birth_year desc, courier_id
