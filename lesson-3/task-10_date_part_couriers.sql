5be8dd53e155af3b5349a5a909203a262b3640ff
Ta2iana <samofalova.ts@gmail.com>
2026-02-16 14:48:13 +0300

    Delete lesson-3 directory

--- lesson-3/task-10_date_part_couriers.sql
+++
@@ -1,7 +0,0 @@
--- Задача 10 (урок 3, karpov.courses SQL):id курьеров и годы рождения из couriers; 
--- birth_year из birth_date; ORDER BY birth_year DESC, courier_id ASC
-
-SELECT courier_id,
-       date_part('year', birth_date) as birth_year
-FROM   couriers
-ORDER BY birth_year desc, courier_id
